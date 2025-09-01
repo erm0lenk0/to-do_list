da0eb8db8a6562ea6e65187e8f47b22a56ec8835
Ermolenko Andrey <f1nd6r@gmail.com>
2025-09-01 16:58:39 +0300

    add admin

--- plans/admin.py
+++ plans/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from plans.models import Task, Tag
 
-# Register your models here.
+admin.site.register(Task)
+admin.site.register(Tag)
--- plans/models.py
+++ plans/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", related_name="tasks")
+
+
+    def __str__(self):
+        return self.content
+
+class Tag(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
